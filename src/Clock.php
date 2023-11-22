81379a96c23bc688de0a74457e0194b5869c4418
Taka Oyama <taka.oyama@gmail.com>
2023-11-23 02:04:24 +0900

    add methods

--- src/Clock.php
+++ src/Clock.php
@@ -14,8 +14,27 @@ class Clock implements ClockInterface
     {
     }
 
+    /**
+     * @return Time
+     */
     public function now(): Time
     {
         return $this->fixed ?? new Time(null, $this->timezone);
+    }
+
+    /**
+     * @return DateTimeZone|null
+     */
+    public function getTimezone(): ?DateTimeZone
+    {
+        return $this->timezone;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isFixed(): bool
+    {
+        return $this->fixed !== null;
     }
 }
